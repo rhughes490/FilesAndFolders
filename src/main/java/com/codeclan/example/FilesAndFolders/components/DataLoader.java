65825ecb69608e637a4b6c2cb64fa0ae7dad77da
Ronan Hughes <rhughes490@gmail.com>
2020-09-08 21:10:04 +0100

    Controllers and DataLoader complete

--- src/main/java/com/codeclan/example/FilesAndFolders/components/DataLoader.java
+++ src/main/java/com/codeclan/example/FilesAndFolders/components/DataLoader.java
@@ -1,4 +1,68 @@
 package com.codeclan.example.FilesAndFolders.components;
 
-public class DataLoader {
+import com.codeclan.example.FilesAndFolders.models.File;
+import com.codeclan.example.FilesAndFolders.models.Folder;
+import com.codeclan.example.FilesAndFolders.models.User;
+import com.codeclan.example.FilesAndFolders.repositories.FileRepository;
+import com.codeclan.example.FilesAndFolders.repositories.FolderRepository;
+import com.codeclan.example.FilesAndFolders.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    FileRepository fileRepository;
+
+    @Autowired
+    FolderRepository folderRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    public DataLoader(){}
+
+    public void run(ApplicationArguments args) {
+
+        User user1 = new User("Joe Blogs");
+        userRepository.save(user1);
+
+        User user2 = new User("John Blogs");
+        userRepository.save(user2);
+
+        User user3 = new User("Betty Blogs");
+        userRepository.save(user3);
+
+        User user4 = new User("Barbara Blogs");
+        userRepository.save(user4);
+
+        Folder folder1 = new Folder("Work", user1);
+        folderRepository.save(folder1);
+
+        Folder folder2 = new Folder("Personal", user1);
+        folderRepository.save(folder2);
+
+        Folder folder3 = new Folder("Hobbies", user1);
+        folderRepository.save(folder3);
+
+        Folder folder4 = new Folder("Misc", user1);
+        folderRepository.save(folder4);
+
+        File file1 = new File("ToDo", "DOC", 50, folder1);
+        fileRepository.save(file1);
+
+        File file2 = new File("HolidayPhoto", "JPG", 20, folder2);
+        fileRepository.save(file2);
+
+        File file3 = new File("Fishing", "PPT", 100, folder3);
+        fileRepository.save(file1);
+
+        File file4 = new File("Letter", "PDF", 7, folder4);
+        fileRepository.save(file4);
+
+    }
+
 }
--- src/main/java/com/codeclan/example/FilesAndFolders/repositories/FolderRepository.java
+++ src/main/java/com/codeclan/example/FilesAndFolders/repositories/FolderRepository.java
@@ -1,4 +1,9 @@
 package com.codeclan.example.FilesAndFolders.repositories;
 
-public interface FolderRepository {
+import com.codeclan.example.FilesAndFolders.models.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FolderRepository extends JpaRepository<Folder, Long> {
 }
